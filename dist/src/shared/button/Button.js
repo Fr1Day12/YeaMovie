1eb93faee4b52a476492fb552b318ab5220849a8
Almaz Letfullin <almaz9061198856@gmail.com>
2024-09-29 23:42:32 +0300

    prod test

--- dist/src/shared/button/Button.js
+++
@@ -1,8 +0,0 @@
-import { jsxs as _jsxs } from "react/jsx-runtime";
-import * as classes from "./classes.module.scss";
-import cn from "classnames";
-var Button = function (_a) {
-    var text = _a.text, onClick = _a.onClick, className = _a.className, children = _a.children;
-    return (_jsxs("button", { onClick: onClick, className: cn(classes.button, className), children: [children, text] }));
-};
-export default Button;
