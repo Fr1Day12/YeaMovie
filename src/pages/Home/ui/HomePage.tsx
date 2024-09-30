6a2f7e991ba271bf5e60f7ed86d2a623ab2da2f6
Almaz Letfullin <almaz9061198856@gmail.com>
2024-09-30 20:24:04 +0300

    Homep

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,6 +1,5 @@
 import StreamSection from "@/pages/Home/ui/Stream/ui/StreamSection";
 import Questions from "@/pages/Home/ui/Questions/ui/Questions";
-import SectionHeader from "@/pages/Home/ui/SectionHeader/ui/SectionHeader";
 import Plan from "@/widgets/Plan/ui/Plan";
 import HomePageSlider from "@/pages/Home/ui/HomePageSlider/ui/HomePageSlider";
 
